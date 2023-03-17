caf9fdd97c6069a29dec267600ee3d6257c22b42
MikeWealth18 <wealthmichael559@gmail.com>
2023-03-16 21:27:33 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - c program that prints a line
+ *
+ * Return: 1 (successful)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
