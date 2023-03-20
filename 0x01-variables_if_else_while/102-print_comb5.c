42ae207f4471378b8871fde2643f626bbaeac30c
MikeWealth18 <wealthmichael559@gmail.com>
2023-03-20 14:22:37 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,9 @@ int main(void)
 	int p;
 	int q;
 
-	for (p = 0 ; p <= 99 ; p++)
+	for (p = 0 ; p <= 98 ; p++)
 	{
-		for (q = p + 1 ; q <= 98 ; q++)
+		for (q = p + 1 ; q <= 99 ; q++)
 		{
 			putchar((p / 10) + '0');
 			putchar((p % 10) + '0');
@@ -24,7 +24,7 @@ int main(void)
 			putchar((q % 10) + '0');
 			if (p == 98 && q == 99)
 				continue;
-			putchar('.');
+			putchar(',');
 			putchar(' ');
 		}
 	}
